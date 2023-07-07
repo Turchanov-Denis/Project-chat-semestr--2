0d9f101f1f6ae106d69d4d486b34c67fc14cf8b0
Turchanov-Denis <turchanovdenis786@gmail.com>
2023-07-07 16:20:47 +0200

    added exception handling

--- server/main.cpp
+++ server/main.cpp
@@ -8,22 +8,27 @@ using json = nlohmann::json;
 
 class ServerManager {
 public:
-    ServerManager(const std::string& dbFilePath, const std::string& logFile) : databaseManager_(dbFilePath, logFile) {
+    ServerManager(const std::string& dbFilePath, const std::string& logFile) : databaseManager_(dbFilePath,logFile), logger_(logFile) {
         server_ = std::make_unique<httplib::Server>();
         setupRoutes();
     }
 
     void start(int port) {
-        if (server_->listen("localhost", port)) {
-            std::cout << "Сервер запущен на порту " << port << std::endl;
-        } else {
-            std::cerr << "Не удалось запустить сервер" << std::endl;
+        try {
+            if (server_->listen("localhost", port)) {
+                std::cout << "Сервер запущен на порту " << port << std::endl;
+            } else {
+                std::cerr << "Не удалось запустить сервер" << std::endl;
+            }
+        } catch (const std::exception& ex) {
+            std::cerr << "Ошибка при запуске сервера: " << ex.what() << std::endl;
         }
     }
 
 private:
     std::unique_ptr<httplib::Server> server_;
     DatabaseManager databaseManager_;
+    Logger logger_;
 
     static void enableCORS(const httplib::Request& req, httplib::Response& res) {
         res.set_header("Access-Control-Allow-Origin", "*");
@@ -35,9 +40,18 @@ private:
         res.set_header("Access-Control-Allow-Origin", "*");
         res.set_header("Content-Type", "application/json");
 
-        json messages = databaseManager_.getMessages();
-
-        res.set_content(messages.dump(), "application/json");
+        try {
+            json messages = databaseManager_.getMessages();
+            res.set_content(messages.dump(), "application/json");
+        } catch (const DatabaseException& ex) {
+            std::string errorMsg = "Ошибка при получении сообщений из базы данных: " + std::string(ex.what());
+            res.set_content(errorMsg, "text/plain");
+            logger_.log(errorMsg);
+        } catch (const std::exception& ex) {
+            std::string errorMsg = "Ошибка при получении сообщений из базы данных: " + std::string(ex.what());
+            res.set_content(errorMsg, "text/plain");
+            logger_.log(errorMsg);
+        }
     }
 
     void postMessage(const httplib::Request& req, httplib::Response& res) {
@@ -46,10 +60,19 @@ private:
         std::string characterName = request["characterName"];
         std::string time = request["time"];
 
-        databaseManager_.addMessage(message, characterName, time);
-
-        res.set_header("Access-Control-Allow-Origin", "*");
-        res.set_content("Сообщение успешно добавлено", "text/plain");
+        try {
+            databaseManager_.addMessage(message, characterName, time);
+            res.set_header("Access-Control-Allow-Origin", "*");
+            res.set_content("Сообщение успешно добавлено", "text/plain");
+        } catch (const DatabaseException& ex) {
+            std::string errorMsg = "Ошибка при добавлении сообщения в базу данных: " + std::string(ex.what());
+            res.set_content(errorMsg, "text/plain");
+            logger_.log(errorMsg);
+        } catch (const std::exception& ex) {
+            std::string errorMsg = "Ошибка при добавлении сообщения в базу данных: " + std::string(ex.what());
+            res.set_content(errorMsg, "text/plain");
+            logger_.log(errorMsg);
+        }
     }
 
     void setupRoutes() {
@@ -64,8 +87,12 @@ private:
 };
 
 int main() {
-    ServerManager serverManager("chat.db", "server.log");
-    serverManager.start(8080);
+    try {
+        ServerManager serverManager("chat.db", "server.log");
+        serverManager.start(8080);
+    } catch (const std::exception& ex) {
+        std::cerr << "Ошибка при запуске сервера: " << ex.what() << std::endl;
+    }
 
     return 0;
 }
